1f576eaa79b7129e0877445ce56936dbfbd7331e
huni <xogns12356@gmail.com>
2021-01-22 02:51:41 +0900

    Create static admin

--- config/settings.py
+++ config/settings.py
@@ -129,9 +129,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
