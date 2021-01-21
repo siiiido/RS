347e6cc3308deb7978de8d0a65ad2f023e1f70dd
huni <xogns12356@gmail.com>
2021-01-22 03:01:35 +0900

    static files

--- config/settings.py
+++ config/settings.py
@@ -130,8 +130,8 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATIC_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
