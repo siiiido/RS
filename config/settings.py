2a96ad436b83f5025d4087dee31266e61ac6fd7b
huni <xogns12356@gmail.com>
2021-01-23 14:39:41 +0900

    Test commit 7

--- config/settings.py
+++ config/settings.py
@@ -129,4 +129,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL='/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 STATIC_ROOT=os.path.join(BASE_DIR, '.static')
