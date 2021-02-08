e5849428636d9d5e6c36ef9f8236bb5669d314ba
SangHan <discegaudere24.7@gmail.com>
2021-02-08 23:34:41 +0900

    modified submit

--- submit/views.py
+++ submit/views.py
@@ -6,6 +6,27 @@ from django.shortcuts import render
 
 def submit(request):
     if request.method == "GET":
+        print("get")
         return render(request, 'submit/submit.html')
-    # elif request.method == "POST" :
-    #     return 
+    elif request.method == "POST" :
+        kakao = request.POST.get('kakao')
+        uni_photo = request.POST.get('uni-photo')
+        cc_radio = request.POST.get('cc_radio')
+
+
+        question=[]
+
+        for i in range(1,11):
+            question.append(request.POST.get('q'+str(i)))
+
+
+
+        # print("카카오 id :" + kakao,"학생증 사진 : " + uni_photo, "cc_radio : "+cc_radio)
+        print("카카오 : {}, 학생증 사진 : {}, cc확인 : {}".format(kakao,uni_photo,cc_radio))
+
+        print("2지 선다 답 : {}".format(question))
+
+        
+        # context = {}
+        # print("post 이제 여기서 어떻게 하는가")
+        return render(request,'submit/submit.html') 
--- templates/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('new/', views.new, name='new'),
-#     path('detail/<int:blog_id>/', views.detail, name='detail'),
-#     path('create/', views.create, name='create'),
-#     path('postcreate/', views.postcreate, name='postcreate'),
-# ]
