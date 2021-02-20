870dfb2a03af939f972a56605d0b2476e348387b
SangHan <discegaudere24.7@gmail.com>
2021-02-20 22:19:43 +0900

    modified1

--- submit/views.py
+++ submit/views.py
@@ -14,7 +14,7 @@ def submit(request):
         if user :
             social_user = Social_User_Table.objects.get(pk=user)
             context = {'user' : social_user}
-            return render(request, 'submit/submit_test.html', context)
+            return render(request, 'submit/submit.html', context)
         
         else :            
             return redirect('/')      
@@ -25,22 +25,43 @@ def submit(request):
         2. 데이터 DB 반영
         3. 페이지 전환
         """
-        html_data = request.POST.get('title')
-        print(html_data)
 
         
 
-        user = request.session.get('user')
-        print(user)
-        social_user = Social_User_Table.objects.get(pk=user)
+        # user = request.session.get('user')
+        # print(user)
 
-        Social_User_Table(
-                user_id         = user,
-                contact         = html_data,
-            ).save()
+        html_user_nickname = request.POST.get('html_user_nickname','')
+        print(html_user_nickname)
+        print("asdsa")
+
+        html_university = request.POST.get('html_university',None)
+        print(html_university)
+
+        html_contact = request.POST.get('html_contact')
+        print("html_contact : ",html_contact)
+
+        html_image = request.POST.get('html_image')
+        print(html_image)
+
+        html_preference = request.POST.get('html_preference','')
+        print("html_preference : ",html_preference)
+
+        html_Q01 = request.POST.get('html_Q01')
+        print(html_Q01)
+
+
+
+
+        # social_user = Social_User_Table.objects.get(pk=user)
+
+        # Social_User_Table(
+        #         user_id         = user,
+        #         contact         = html_data,
+        #     ).save()
         
 
         
         print("post fin")
 
-        return render(request, "submit/submit_test.html")
+        return redirect('/status')
