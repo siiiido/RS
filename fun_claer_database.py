f6d7207edd8a557e05a8c33cc092933bfcd207c8
huni <xogns12356@gmail.com>
2021-03-02 18:52:05 +0900

    Modified clear_database.py 2021-03-02

--- fun_claer_database.py
+++ fun_claer_database.py
@@ -1,7 +1,7 @@
-
 import django
 import os
 import random
+import shutil
 from operator import attrgetter
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -11,9 +11,18 @@ from social.models import Social_User_Table
 
 
 def process_clear():
+    delete_social_user_table()
+    delete_image_files()
+
+
+def delete_social_user_table():
     database_all = Social_User_Table.objects.all()
     for data in database_all:
         data.delete()
-        
+
+def delete_image_files():
+    shutil.rmtree('media')
+    os.makedirs('media')
+
 
 process_clear()
